601233dd15d9fc235b72f493b35e98c77a6bbddf
Israel Felix <israel.felix193@tectijuana.edu.mx>
2023-04-23 02:44:17 -0700

    2-3 Eventos agregados
    Aun falta crear los templates y alinear los cuadros en horizontal para mayor presentacion

---
+++ ejercicioJS/animaciones.js
@@ -0,0 +1,22 @@
+const boton = document.querySelector('button');
+const bloque = document.querySelectorAll('section');
+//CLICK EVENT
+boton.addEventListener('click',function()
+{
+    alert('No aprietes mas el boton por favor');
+   
+});
+boton.addEventListener('click',function()
+{
+    alert('Es muy molesto tener que poner estas alertas');
+   
+});
+//MOUSEOVER Y MOUSEOUT EVENTS
+bloque.forEach(function(item){
+    item.addEventListener('mouseover',function(){
+        item.style.backgroundColor=item.dataset.color || "purple";
+    });
+    item.addEventListener('mouseout',function(){
+        item.style.backgroundColor="gray";})
+
+})
