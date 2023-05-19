aa879e150ede8ad14a6b730244c5304960dc2ed0
P-once <andres.ponce193@tectijuana.edu.mx>
2023-05-18 18:04:32 -0700

    o

--- ProcastinVistas/sever.php
+++ ProcastinVistas/sever.php
@@ -7,13 +7,13 @@ $email = "";
 $errors = array();
 
 //connexion a db
-$db = mysqli_connect('localhost', 'root', '', 'project');
+$db = mysqli_connect('localhost', 'root', '', 'procastin');
 
 //Registrar usuario
 if(isset($_POST['reg_user'])) {
 	//Valores recibidos del form
-	$username = mysqli_real_escape_string($db, $_POST['username']);
-	$email = mysqli_real_escape_string($db, $_POST['email']);
+	$username = mysqli_real_escape_string($db, $_POST['usuario']);
+	$email = mysqli_real_escape_string($db, $_POST['correo']);
 	$password = mysqli_real_escape_string($db, $_POST['password']);
 
 	if(empty($username)) { array_push($errors, "Nombre de usuario es requerrido"); }
@@ -24,11 +24,11 @@ if(isset($_POST['reg_user'])) {
 	$user_check_query($db, $user_check_query);
 	$user = mysqli_fetch_assoc($result);
 
-	if($user['username'] === $username) {
+	if($user['NomUsuario'] === $username) {
 		array_push($errors, "Username already exists");
 	}
 
-	if($user['email'] === $email) {
+	if($user['CorreoUsuario'] === $email) {
 		array_push($errors, "email already exists");
 	}
 
@@ -47,26 +47,27 @@ if(isset($_POST['reg_user'])) {
 
 // LOGIN USER
 if (isset($_POST['login_user'])) {
-	$username = mysqli_real_escape_string($db, $_POST['username']);
+	$username = mysqli_real_escape_string($db, $_POST['usuario']);
 	$password = mysqli_real_escape_string($db, $_POST['password']);
+
+	$password = md5($password);
   
 	if (empty($username)) {
-		array_push($errors, "Username is required");
+		array_push($errors, "Se requiere nombre de usuario");
 	}
 	if (empty($password)) {
-		array_push($errors, "Password is required");
+		array_push($errors, "Se requiere contraseña de usuario");
 	}
   
 	if (count($errors) == 0) {
-		$password = md5($password);
-		$query = "SELECT * FROM usuarios WHERE NomUsuario='$username' AND password='$password'";
+		$query = "SELECT * FROM usuarios WHERE NomUsuario='$username' AND ContraseniaUsuario='$password'";
 		$results = mysqli_query($db, $query);
 		if (mysqli_num_rows($results) == 1) {
 		  $_SESSION['username'] = $username;
 		  $_SESSION['success'] = "You are now logged in";
 		  header('location: PerfilUsuario.html');
 		}else {
-			array_push($errors, "Wrong username/password combination");
+			array_push($errors, "Combinacion incorrecta de usuario/contraseña");
 		}
 	}
   }
